f4219bf6b88d4b8300fffe3cb1148c11d5c60a91
tombjorn <118115159+tombjorn@users.noreply.github.com>
2022-12-01 15:21:01 +0000

    Create Problem_Gen.py

---
+++ Problem_Gen.py
@@ -0,0 +1,17 @@
+import random
+import choices
+
+# length of Problem dict
+n = len(choices.ProbType)
+# random value from Problem dict
+r = random.randint(1, n)
+
+# open with the interntion of writing
+File_object = open("Problem_File.py", "w")
+
+# wrie in problem
+File_object.write('# {} \n'.format(choices.ProbType[r]))
+File_object.write('{}'.format(choices.fileWriter(r)))
+
+# oh malloc you
+File_object.close()
